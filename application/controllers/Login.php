59430709f95db32312ce894980490bd63986e2f2
Bas van Marwijk <12039179@student.hhs.nl>
2016-04-08 17:09:06 +0200

    Remove duplicate view loads

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -7,9 +7,7 @@ class Login extends CI_Controller
 {
     public function index()
     {
-        $this->load->view('header');
         $this->load->view('login');
-        $this->load->view('footer');
     }
 }
 ?>
