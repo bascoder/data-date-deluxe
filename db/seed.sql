e5dff2411f56225bde6ffc850c99e4b32f443747
Bas van Marwijk <12039179@student.hhs.nl>
2016-04-12 21:05:38 +0200

    Seed.sql point to correct URL

--- db/seed.sql
+++ db/seed.sql
@@ -5,7 +5,7 @@ INSERT INTO `Geslacht` (gid, geslacht)
 VALUES (1, 'man'), (2, 'vrouw');
 
 INSERT INTO `Foto`(fid, url, titel, beschrijving)
-VALUES (1, 'assets/img/profiel_fotos/placeholder_female.svg', 'placeholder man', 'Ik heb nog geen profiel foto'),
-  (2, 'assets/img/profiel_fotos/placeholder_male.svg', 'placeholder vrouw', 'Ik heb nog geen profiel foto');
+VALUES (1, 'assets/img/profiel_fotos/placeholder_male.svg', 'placeholder man', 'Ik heb nog geen profiel foto'),
+  (2, 'assets/img/profiel_fotos/placeholder_female.svg', 'placeholder vrouw', 'Ik heb nog geen profiel foto');
 
 COMMIT;
