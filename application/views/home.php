0e7226a0023b223ec8976109a9513982732e1f91
Bas van Marwijk <12039179@student.hhs.nl>
2016-04-13 12:29:41 +0200

    Home page general info

--- application/views/home.php
+++ application/views/home.php
@@ -1,1 +1,13 @@
 <h1>Data Date Deluxe</h1>
+<article class="readable">
+    <?php if (!is_ingelogd()): ?>
+        <p>Toe aan een relatie? Een vaste? Of juist
+            casual? <strong><?php echo anchor('register', 'Meldt u nu aan voor Data Date Deluxe!') ?></strong></p>
+        <p>
+            Dankzij deze website hebben al duizenden stelletjes hun geluk gevonden. Na het aanmelden voor een profiel
+            zullen onze algoritmes de beste matches voor uw specifieke persoonlijkheids type uitzoeken.
+            Zodra een wederzijdse like geeft kunt u elkaars contact informatie zien, en kan het <strong>daten</strong>
+            beginnen!
+        </p>
+    <?php endif; ?>
+</article>
