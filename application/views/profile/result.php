fafb9a4246a710b63fdac9bde4ea31765af47153
Bas van Marwijk <12039179@student.hhs.nl>
2016-04-14 20:44:38 +0200

    Correct profile picture in results

--- application/views/profile/result.php
+++ application/views/profile/result.php
@@ -1,22 +1,24 @@
 <div>
     <?php
-    if (current_privileges() === Authentication::ANONYMOUS):
 
-        foreach ($profielen as $profiel):
-            echo "<div class='profiel'>";
-            echo img($profiel->profiel_foto->url, FALSE, 'class="profiel-foto"');
-            echo '<table>';
-            echo "<tr><td>Nickname</td><td>$profiel->nickname</td></tr>";
-            $geslacht = $profiel->geslacht->geslacht;
-            echo "<tr><td>Geslacht</td><td>$geslacht</td></tr>";
-            $leeftijd = floor((time() - ($profiel->geboorte_datum)) / 31556926);
-            echo "<tr><td>Leeftijd</td><td>$leeftijd</td></tr>";
-            echo "<tr><td>Beschrijving</td><td>$profiel->beschrijving</td></tr>";
-            echo "<tr><td>Persoonlijkheids type</td><td></td></tr>";
-            echo '</table>';
-            echo '</div>';
-        endforeach;
+    foreach ($profielen as $profiel):
+        echo "<div class='profiel'>";
+        $geslacht = $profiel->geslacht->geslacht;
+        if (current_privileges() === Authentication::ANONYMOUS):
+            echo img(placeholder_url($geslacht), FALSE, 'class="profiel-foto" alt="log in om een profiel foto te bekijken"');
+        else:
+            echo img($profiel->profiel_foto->url, FALSE, 'class="profiel-foto" alt="profiel foto"');
+        endif;
+        echo '<table>';
+        echo "<tr><td>Nickname</td><td>$profiel->nickname</td></tr>";
+        echo "<tr><td>Geslacht</td><td>$geslacht</td></tr>";
+        $leeftijd = floor((time() - ($profiel->geboorte_datum)) / 31556926);
+        echo "<tr><td>Leeftijd</td><td>$leeftijd</td></tr>";
+        echo "<tr><td>Beschrijving</td><td>$profiel->beschrijving</td></tr>";
+        echo "<tr><td>Persoonlijkheids type</td><td></td></tr>";
+        echo '</table>';
+        echo '</div>';
+    endforeach;
 
-    endif;
     ?>
 </div>
