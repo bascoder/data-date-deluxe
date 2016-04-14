60e7ce419776217d454c0cd3e34c191da7b004cf
Bas van Marwijk <12039179@student.hhs.nl>
2016-04-14 20:33:32 +0200

    Profile search result page

--- application/views/profile/result.php
+++ application/views/profile/result.php
@@ -1,2 +1,22 @@
-<?php
-var_dump($profielen);
+<div>
+    <?php
+    if (current_privileges() === Authentication::ANONYMOUS):
+
+        foreach ($profielen as $profiel):
+            echo "<div class='profiel'>";
+            echo img($profiel->profiel_foto->url, FALSE, 'class="profiel-foto"');
+            echo '<table>';
+            echo "<tr><td>Nickname</td><td>$profiel->nickname</td></tr>";
+            $geslacht = $profiel->geslacht->geslacht;
+            echo "<tr><td>Geslacht</td><td>$geslacht</td></tr>";
+            $leeftijd = floor((time() - ($profiel->geboorte_datum)) / 31556926);
+            echo "<tr><td>Leeftijd</td><td>$leeftijd</td></tr>";
+            echo "<tr><td>Beschrijving</td><td>$profiel->beschrijving</td></tr>";
+            echo "<tr><td>Persoonlijkheids type</td><td></td></tr>";
+            echo '</table>';
+            echo '</div>';
+        endforeach;
+
+    endif;
+    ?>
+</div>
