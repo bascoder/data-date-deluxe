b6056029df9f48802c3c65814121b2e17d40449e
Bas van Marwijk <12039179@student.hhs.nl>
2016-04-20 14:54:33 +0200

    Even more fail safe seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -4,6 +4,7 @@ BEGIN TRANSACTION;
 INSERT INTO `Geslacht` (gid, geslacht)
 VALUES (1, 'man'), (2, 'vrouw');
 
+-- fid van placeholders zijn gelijk aan gid van Geslacht, hier wordt op gerekend door de applicatie
 INSERT INTO `Foto` (fid, url, titel, beschrijving)
 VALUES (1, 'assets/img/profiel_fotos/placeholder_male.svg', 'placeholder man', 'Ik heb nog geen profiel foto'),
   (2, 'assets/img/profiel_fotos/placeholder_female.svg', 'placeholder vrouw', 'Ik heb nog geen profiel foto');
@@ -27,8 +28,9 @@ VALUES (1, 'ISTJ', 'Inspector'),
   (16, 'ENTJ', 'Fieldmarshal');
 
 -- test data
-INSERT INTO `Profiel` (pid, voornaam, achternaam, email, password, is_admin, nickname, beschrijving, geboorte_datum,
-                       leeftijd_voorkeur_min, leeftijd_voorkeur_max, valt_op_man, valt_op_vrouw, geslacht_id)
+-- OR IGNORE clause zorgt ervoor dat rows die niet aan constraints voldoen (door random) overgeslagen worden
+INSERT OR IGNORE INTO `Profiel` (pid, voornaam, achternaam, email, password, is_admin, nickname, beschrijving, geboorte_datum,
+                                 leeftijd_voorkeur_min, leeftijd_voorkeur_max, valt_op_man, valt_op_vrouw, geslacht_id)
 VALUES (1, 'Bas', 'van Marwijk', 'bas@example.com', '$2y$10$hL0wVNcja/nS3us93I4fau/4RKiL6whA7P73Uny6HRNESEP3odRBK', 1,
            'bas1994', 'Pro admin', 782694000, 18, 25, 0, 1, 1),
   (2, 'Henk', 'de Jong', 'henk@example.com', '$2y$10$LyzwMXTyXdRO7.9jTOlcaegIlZQuN5pqrAn4X9oNi4mV8rn0NauMq', 0,
@@ -49,7 +51,7 @@ VALUES (1, 'Bas', 'van Marwijk', 'bas@example.com', '$2y$10$hL0wVNcja/nS3us93I4f
       'tonnie', '<3', 752694000, 18, 30, 1, 1, 1),
   (10, 'Timmie', 'Hengel', 'timmie@hotmail.com', '$2y$10$LyzwMXTyXdRO7.9jTOlcaegIlZQuN5pqrAn4X9oNi4mV8rn0NauMq', 0,
        'tim3', '<3', 732694000, ABS(RANDOM() % 5) + 18, ABS(RANDOM() % 5) + 27, ABS(RANDOM() % 1), 1,
-    ABS(random() % 2) + 1),
+   ABS(random() % 2) + 1),
   (11, 'Tijs', 'Hengel', 'tijs@hotmail.com', '$2y$10$LyzwMXTyXdRO7.9jTOlcaegIlZQuN5pqrAn4X9oNi4mV8rn0NauMq', 0,
        'tisjeboy', '<3', 732694000, ABS(RANDOM() % 5) + 18, ABS(RANDOM() % 5) + 27, ABS(RANDOM() % 1), 1,
    ABS(random() % 2) + 1),
@@ -81,58 +83,56 @@ VALUES (1, 'Bas', 'van Marwijk', 'bas@example.com', '$2y$10$hL0wVNcja/nS3us93I4f
        'nadia', '<3', 732694000, ABS(RANDOM() % 5) + 18, ABS(RANDOM() % 5) + 27, 1, ABS(RANDOM() % 1),
    ABS(random() % 2) + 1);
 
-INSERT INTO `Merk` (naam)
-VALUES
-  ('Apple'),
-  ('Microsoft'),
-  ('Google'),
-  ('Coca-Cola'),
-  ('IBM'),
-  ('McDonald''s'),
-  ('Samsung'),
-  ('Toyota'),
-  ('General Electric'),
-  ('Facebook'),
-  ('Disney'),
-  ('AT&T'),
-  ('Amazon.com'),
-  ('Louis Vuitton'),
-  ('Cisco'),
-  ('BMW'),
-  ('Oracle'),
-  ('NIKE'),
-  ('Intel'),
-  ('Wal-Mart'),
-  ('Verizon'),
-  ('American Express'),
-  ('Honda'),
-  ('Mercedes-Benz'),
-  ('Budweiser'),
-  ('Gillette'),
-  ('Marlboro'),
-  ('Pepsi'),
-  ('Visa'),
-  ('Nescafe'),
-  ('ESPN'),
-  ('H&M'),
-  ('L''Oréal'),
-  ('Hewlett-Packard'),
-  ('HSBC'),
-  ('Home Depot'),
-  ('Frito-Lay'),
-  ('Audi'),
-  ('UPS'),
-  ('Starbucks'),
-  ('Ford'),
-  ('Gucci'),
-  ('Nestle'),
-  ('Accenture'),
-  ('IKEA'),
-  ('Siemens'),
-  ('Wells Fargo'),
-  ('Fox'),
-  ('Pampers'),
-  ('Ebay');
+INSERT INTO "Merk" (mid, naam) VALUES (1, 'Apple');
+INSERT INTO "Merk" (mid, naam) VALUES (2, 'Microsoft');
+INSERT INTO "Merk" (mid, naam) VALUES (3, 'Google');
+INSERT INTO "Merk" (mid, naam) VALUES (4, 'Coca-Cola');
+INSERT INTO "Merk" (mid, naam) VALUES (5, 'IBM');
+INSERT INTO "Merk" (mid, naam) VALUES (6, 'McDonald''s');
+INSERT INTO "Merk" (mid, naam) VALUES (7, 'Samsung');
+INSERT INTO "Merk" (mid, naam) VALUES (8, 'Toyota');
+INSERT INTO "Merk" (mid, naam) VALUES (9, 'General Electric');
+INSERT INTO "Merk" (mid, naam) VALUES (10, 'Facebook');
+INSERT INTO "Merk" (mid, naam) VALUES (11, 'Disney');
+INSERT INTO "Merk" (mid, naam) VALUES (12, 'AT&T');
+INSERT INTO "Merk" (mid, naam) VALUES (13, 'Amazon.com');
+INSERT INTO "Merk" (mid, naam) VALUES (14, 'Louis Vuitton');
+INSERT INTO "Merk" (mid, naam) VALUES (15, 'Cisco');
+INSERT INTO "Merk" (mid, naam) VALUES (16, 'BMW');
+INSERT INTO "Merk" (mid, naam) VALUES (17, 'Oracle');
+INSERT INTO "Merk" (mid, naam) VALUES (18, 'NIKE');
+INSERT INTO "Merk" (mid, naam) VALUES (19, 'Intel');
+INSERT INTO "Merk" (mid, naam) VALUES (20, 'Wal-Mart');
+INSERT INTO "Merk" (mid, naam) VALUES (21, 'Verizon');
+INSERT INTO "Merk" (mid, naam) VALUES (22, 'American Express');
+INSERT INTO "Merk" (mid, naam) VALUES (23, 'Honda');
+INSERT INTO "Merk" (mid, naam) VALUES (24, 'Mercedes-Benz');
+INSERT INTO "Merk" (mid, naam) VALUES (25, 'Budweiser');
+INSERT INTO "Merk" (mid, naam) VALUES (26, 'Gillette');
+INSERT INTO "Merk" (mid, naam) VALUES (27, 'Marlboro');
+INSERT INTO "Merk" (mid, naam) VALUES (28, 'Pepsi');
+INSERT INTO "Merk" (mid, naam) VALUES (29, 'Visa');
+INSERT INTO "Merk" (mid, naam) VALUES (30, 'Nescafe');
+INSERT INTO "Merk" (mid, naam) VALUES (31, 'ESPN');
+INSERT INTO "Merk" (mid, naam) VALUES (32, 'H&M');
+INSERT INTO "Merk" (mid, naam) VALUES (33, 'L''Or├®al');
+INSERT INTO "Merk" (mid, naam) VALUES (34, 'Hewlett-Packard');
+INSERT INTO "Merk" (mid, naam) VALUES (35, 'HSBC');
+INSERT INTO "Merk" (mid, naam) VALUES (36, 'Home Depot');
+INSERT INTO "Merk" (mid, naam) VALUES (37, 'Frito-Lay');
+INSERT INTO "Merk" (mid, naam) VALUES (38, 'Audi');
+INSERT INTO "Merk" (mid, naam) VALUES (39, 'UPS');
+INSERT INTO "Merk" (mid, naam) VALUES (40, 'Starbucks');
+INSERT INTO "Merk" (mid, naam) VALUES (41, 'Ford');
+INSERT INTO "Merk" (mid, naam) VALUES (42, 'Gucci');
+INSERT INTO "Merk" (mid, naam) VALUES (43, 'Nestle');
+INSERT INTO "Merk" (mid, naam) VALUES (44, 'Accenture');
+INSERT INTO "Merk" (mid, naam) VALUES (45, 'IKEA');
+INSERT INTO "Merk" (mid, naam) VALUES (46, 'Siemens');
+INSERT INTO "Merk" (mid, naam) VALUES (47, 'Wells Fargo');
+INSERT INTO "Merk" (mid, naam) VALUES (48, 'Fox');
+INSERT INTO "Merk" (mid, naam) VALUES (49, 'Pampers');
+INSERT INTO "Merk" (mid, naam) VALUES (50, 'Ebay');
 
 -- OR IGNORE clause zorgt ervoor dat rows die niet aan constraints voldoen (door random) overgeslagen worden
 INSERT OR IGNORE INTO Merk_voorkeur (merk_id, profiel_id)
@@ -160,28 +160,25 @@ VALUES (4, 5), (1, 5),
   (ABS(RANDOM() % 49) + 1, ABS(RANDOM() % 18) + 1),
   (ABS(RANDOM() % 49) + 1, ABS(RANDOM() % 18) + 1);
 
--- OR IGNORE clause zorgt ervoor dat rows die niet aan constraints voldoen (door random) overgeslagen worden
 UPDATE OR IGNORE Profiel
 SET persoonlijkheids_type_id = ABS(RANDOM() % 15) + 1;
 
--- OR IGNORE clause zorgt ervoor dat rows die niet aan constraints voldoen (door random) overgeslagen worden
 UPDATE OR IGNORE Profiel
 SET persoonlijkheids_type_voorkeur_id = ABS(RANDOM() % 15) + 1;
 
--- OR IGNORE clause zorgt ervoor dat rows die niet aan constraints voldoen (door random) overgeslagen worden
-INSERT OR IGNORE INTO Like(liker_id, liked_id)
-VALUES ((ABS(RANDOM() %18) + 1),(ABS(RANDOM() %18) + 1)),
-  ((ABS(RANDOM() %18) + 1),(ABS(RANDOM() %18) + 1)),
-  ((ABS(RANDOM() %18) + 1),(ABS(RANDOM() %18) + 1)),
-  ((ABS(RANDOM() %18) + 1),(ABS(RANDOM() %18) + 1)),
-  ((ABS(RANDOM() %18) + 1),(ABS(RANDOM() %18) + 1)),
-  ((ABS(RANDOM() %18) + 1),(ABS(RANDOM() %18) + 1)),
-  ((ABS(RANDOM() %18) + 1),(ABS(RANDOM() %18) + 1)),
-  ((ABS(RANDOM() %18) + 1),(ABS(RANDOM() %18) + 1)),
-  ((ABS(RANDOM() %18) + 1),(ABS(RANDOM() %18) + 1)),
-  ((ABS(RANDOM() %18) + 1),(ABS(RANDOM() %18) + 1)),
-  ((ABS(RANDOM() %18) + 1),(ABS(RANDOM() %18) + 1)),
-  ((ABS(RANDOM() %18) + 1),(ABS(RANDOM() %18) + 1)),
-  ((ABS(RANDOM() %18) + 1),(ABS(RANDOM() %18) + 1));
+INSERT OR IGNORE INTO Like (liker_id, liked_id)
+VALUES ((ABS(RANDOM() % 18) + 1), (ABS(RANDOM() % 18) + 1)),
+  ((ABS(RANDOM() % 18) + 1), (ABS(RANDOM() % 18) + 1)),
+  ((ABS(RANDOM() % 18) + 1), (ABS(RANDOM() % 18) + 1)),
+  ((ABS(RANDOM() % 18) + 1), (ABS(RANDOM() % 18) + 1)),
+  ((ABS(RANDOM() % 18) + 1), (ABS(RANDOM() % 18) + 1)),
+  ((ABS(RANDOM() % 18) + 1), (ABS(RANDOM() % 18) + 1)),
+  ((ABS(RANDOM() % 18) + 1), (ABS(RANDOM() % 18) + 1)),
+  ((ABS(RANDOM() % 18) + 1), (ABS(RANDOM() % 18) + 1)),
+  ((ABS(RANDOM() % 18) + 1), (ABS(RANDOM() % 18) + 1)),
+  ((ABS(RANDOM() % 18) + 1), (ABS(RANDOM() % 18) + 1)),
+  ((ABS(RANDOM() % 18) + 1), (ABS(RANDOM() % 18) + 1)),
+  ((ABS(RANDOM() % 18) + 1), (ABS(RANDOM() % 18) + 1)),
+  ((ABS(RANDOM() % 18) + 1), (ABS(RANDOM() % 18) + 1));
 
 COMMIT;
