81c431b5016ad4f35f5475d474c2b7f40e22d021
Bas van Marwijk <12039179@student.hhs.nl>
2016-04-24 20:11:05 +0200

    Header title: ascii will work everywhere anywhere

--- application/views/header.php
+++ application/views/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Data Date Deluxe &#128525;</title>
+    <title>Data Date Deluxe <3</title>
 
     <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url() . 'favicon.ico' ?>">
 
